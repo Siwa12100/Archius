12b9089f1ffb53da4e15a8c0dadc877aeb1e02d8
Siwa12100 <jean.marcillac12@gmail.com>
2024-01-19 07:15:29 +0100

    suppression Verax

--- VeraxShield/VeraxShield/services/authentificationService/IAuthentificationService.cs
+++
@@ -1,8 +0,0 @@
-using VeraxShield.composants.formulaires.modeles;
-
-public interface IAuthentificationService
-{
-    public UtilisateurCourant GetUtilisateur(String nom);
-    public void Connexion(RequeteConnexion requete);
-    public void Inscription(RequeteInscription requete);
-}
