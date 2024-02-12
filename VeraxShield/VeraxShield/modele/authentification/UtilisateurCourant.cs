89b6791031f858e99ec129ee7dc127740c9c6a87
Siwa12100 <jean.marcillac12@gmail.com>
2024-02-12 11:52:52 +0100

    travail sur les services web

--- VeraxShield/VeraxShield/modele/authentification/UtilisateurCourant.cs
+++
@@ -1,6 +0,0 @@
-public class UtilisateurCourant
-{
-    public Dictionary<string, string> Claims { get; set; }
-    public bool EstAuthentifie { get; set; }
-    public string Pseudo { get; set; }
-}
--- VeraxShield/VeraxShield/services/authentificationService/IAuthentificationService.cs
+++
@@ -1,8 +0,0 @@
-using VeraxShield.composants.formulaires.modeles;
-
-public interface IAuthentificationService
-{
-    public Task<UtilisateurCourant> GetUtilisateur(String nom);
-    public Task Connexion(RequeteConnexion requete);
-    public Task Inscription(RequeteInscription requete);
-}
